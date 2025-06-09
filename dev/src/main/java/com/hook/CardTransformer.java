af0de477d64f9234abeebcf9bb3f114b426ff2ee
CreateWater <blackrp001@naver.com>
2025-06-10 01:22:29 +0900

    Load translated Text Only CardLoader
    Co-authored-by: andrew0416 <andrew0416@naver.com>

--- dev/src/main/java/com/hook/CardTransformer.java
+++ dev/src/main/java/com/hook/CardTransformer.java
@@ -24,41 +24,12 @@ public class CardTransformer implements ClassFileTransformer {
                 return byteCode;
             }
 
-            if (className != null && className.replace("/", ".").equals("open.batoru.catalog.CardPreview")) {
-                CtClass cc = pool.get(dottedClassName);
-                CtMethod updateTableMethod = cc.getDeclaredMethod("updateTable");
-                updateTableMethod.insertBefore(getCardPreviewLogic());              
-                byte[] byteCode = cc.toBytecode();
-                cc.detach();
-                return byteCode;
-                
-            }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
         // 바이트코드를 수정하지 않는 경우 원본 바이트코드를 반환
         return classfileBuffer;
-    }
-
-    private String getCardPreviewLogic() {
-    return ""
-        + "try {"
-        + "System.out.println($2);"
-        + "    java.util.Map translation = com.hook.DBHelper.getTranslationByImageSet($2);"
-        + "    if (translation != null) {"
-        + "        String name = (String) translation.get(\"Name\");"
-        + "        String desc = (String) translation.get(\"description\");"
-        + "        String status = (String) translation.get(\"status\");"
-        + "        if (!\"0\".equals(status)) {"
-        + "            if (name != null) $1.setName(\"kr\", name);"
-        + "            if (desc != null) $1.setDescription(\"kr\", desc);"
-        + "        }"
-        + "    }"
-        + "} catch (Exception e) {"
-        + "        System.out.println(\"오류\");"
-        + "}";
     }
 
     private String getCardLoaderLogic() {
@@ -71,9 +42,11 @@ public class CardTransformer implements ClassFileTransformer {
         + "        java.util.Map translation = com.hook.DBHelper.getTranslationByImageSet(imageSet);"
         + "        if (translation != null) {"
         + "            String name = (String) translation.get(\"Name\");"
+        + "            String desc = (String) translation.get(\"description\");"
         + "            String status = (String) translation.get(\"status\");"
         + "            if (!\"0\".equals(status)) {"
         + "                if (name != null) card.setName(\"kr\", name);"
+        + "                if (desc != null) card.setDescription(\"kr\", desc);"
         + "            }"
         + "        }"
         + "    }"
